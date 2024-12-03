1a95c8228c8b9f48eebb2a7b204bb4ef90afb143
RohitoOo <bhambhani.ba@gmail.com>
2024-12-03 15:51:55 -0500

    Updated .gitignore file

---
+++ app/about/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>Rohit Bhambhani </h1>
+
+      <p>Dummy Text</p>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ app/about/team/page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const TeamPage = () => {
+  return (
+    <>
+      <h1> OUR TEAM</h1>
+
+      <p>Team Page</p>
+    </>
+  );
+};
+<h1> OUR TEAM</h1>;
+
+export default TeamPage;
